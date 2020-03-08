be8bd4520714c3ab38144416124a3bfd9916ff67
谷崎公紀 <marutto8@gmail.com>
2020-03-08 12:34:45 +0900

    add データの登録 new create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,20 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new 
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)  #オブジェクトつくってる
+    @item.save                     #newしてsave
+    redirect_to @item              #showに飛ばして確認させる(一覧の場合は"/items")
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, 
+      :seller, :description, :email, :image_url)
+  end
 end
